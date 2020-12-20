24cea0a15a96ede612b6dd6c358563144bbf1544
takuto moriyasu <smart.tact.cheeser.0803@gmail.com>
2020-12-21 00:26:27 +0900

    responsible

--- components/DataGrid.js
+++ components/DataGrid.js
@@ -6,6 +6,9 @@ import Lib from "../static/address_lib";
 import Router from 'next/router';
 import DataItem from './DataItem';
 import { DataGrid } from '@material-ui/data-grid';
+import { Container } from 'next/app';
+import  MediaQuery from "react-responsive";
+
 
 
 class DataGridComp extends Component
@@ -28,12 +31,18 @@ class DataGridComp extends Component
         this.updateDataTable=this.updateDataTable.bind(this);
         this.setDataList=this.setDataList.bind(this);
         this.onSelectTableItem=this.onSelectTableItem.bind(this);
+        this.TimeCompare=this.TimeCompare.bind(this);
         // this.updateDataTable();
-        this.columns=[
+        this.widecolumns=[
             {field:'id',headerName:'StartTime',width:200},
             {field:'themes',headerName:'Theme',width:200},
             {field:'worktime',headerName:'WorkTime',width:200},
             {field:'memo',headerName:'Memo',width:300},
+        ];
+        this.narrowcolumns=[
+            {field:'id',headerName:'StartTime',width:120},
+            {field:'themes',headerName:'Theme',width:120},
+            {field:'worktime',headerName:'WorkTime',width:110},
         ];
     }
 
@@ -78,6 +87,7 @@ class DataGridComp extends Component
             );
             datalist.push({id:data[i].starttime,themes:data[i].themes,worktime:data[i].worktime,memo:data[i].memo});
         }
+        datalist.sort(this.TimeCompare);
         this.setState({dom:domlist});
         this.setState({rows:datalist});
     }
@@ -98,23 +108,44 @@ class DataGridComp extends Component
         this.updateDataTable();
         console.log("before render");
     }
+
+    TimeCompare(a,b)
+    {
+        return Lib.GetDateFromString(b.id)-Lib.GetDateFromString(a.id);
+    }
     render()
     {
         console.log("renderrrr");
         return(
-            <div style={{ height:800,width:950, justifyContent:"center",margin:"0px auto",padding:"20px"}} onChange={this.onSelectTableItem}>
-                <DataGrid rows={this.state.rows} columns={this.columns} pageSize={15} checkboxSelection />
-            </div>
-            // <div>
-            //     <button onClick={this.onRefreshTable}>refresh</button>
-            // <table>
-            //     <tbody>
-            //         {this.state.dom}
-            //     </tbody>
-            // </table>
-            // </div>
-            
+            <Container style={{display:"flex"}}>
+                <MediaQuery query="(max-width:767px)">
+                    <div style={{ height:600,width:350, justifyContent:"center",margin:"0px auto",paddingTop:"20px"}} onChange={this.onSelectTableItem}>
+                        <DataGrid rows={this.state.rows} columns={this.narrowcolumns} pageSize={15} />
+                    </div>
+                </MediaQuery>
+                <MediaQuery query="(min-width:767px)">
+                    <div style={{ height:800,width:950, justifyContent:"center",margin:"0px auto",padding:"20px"}} onChange={this.onSelectTableItem}>
+                        <DataGrid rows={this.state.rows} columns={this.widecolumns} pageSize={15} checkboxSelection />
+                    </div> 
+                </MediaQuery>
+            </Container>
         );
+        if(this.props.isResonsible)
+        {
+              return(
+                <div style={{ height:800,width:750, justifyContent:"center",margin:"0px auto",padding:"20px"}} onChange={this.onSelectTableItem}>
+                    <DataGrid rows={this.state.rows} columns={this.narrowcolumns} pageSize={15} checkboxSelection />
+                </div> 
+              );  
+        }
+        else
+        {
+            return(
+                <div style={{ height:800,width:950, justifyContent:"center",margin:"0px auto",padding:"20px"}} onChange={this.onSelectTableItem}>
+                    <DataGrid rows={this.state.rows} columns={this.widecolumns} pageSize={15} checkboxSelection />
+                </div> 
+            );
+        }
     }
 }
 
--- pages/datalist.js
+++ pages/datalist.js
@@ -1,12 +1,12 @@
 import React,{Component} from 'react';
 import Link from "next/link";
 import Layout from "../components/Layout";
-
+import  MediaQuery from "react-responsive";
 import Account from '../components/Account';
 import DataGridComp from '../components/DataGrid';
 
 export default ()=>(
-    <Layout header="WorkRecorder" title="FireWorks">
+    <Layout header="WorkRecorder" title="FireWorks" style={{margin:"auto",width:"50%"}}>
         <DataGridComp/>
     </Layout>
 );
